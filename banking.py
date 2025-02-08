efb0038f4cb33096d8856b6586da607343a0f91c
Jolenne Chen <jolenne.chen145@gmail.com>
2025-02-08 21:20:18 +0700

    i upgraded my bank

--- banking.py
+++ banking.py
@@ -1,46 +1,60 @@
-def Check_amount(amount):
+name = input("your name: ")
+age = int(input("your age: "))
+if age < 18:
+    raise Exception("Your age can't be below 18")
+elif age > 110 :
+    raise Exception("Old")
+
+print("You have accessed your bank")
+
+amount = int(input("input the amount of money: "))
+if amount < 0:
+    raise Exception("You can't input a negative number")
+
+def Check_amount():
     print(f"amount: {amount}")
 
-def add_money(amount):
+def add_money():
     Add = int(input("Input the money you want to add: "))
+    global amount
     amount = amount + Add
     print(f"amount: {amount}")
 
-def withdraw(amount):
+def withdraw():
     Withdraw = int(input("How much do you want to withdraw? : "))
+    global amount
     if Withdraw > amount:
         raise Exception("You cant withdraw more than you have")
     else:     
         amount = amount - Withdraw
         print(f"amount: {amount}")
 
-try:
-    amount = int(input("input the amount of money: "))
-    if amount < 0:
-        raise Exception("You can't input a negative number")
+def main():
 
-    function = int(input(print('''
-     Do you want to :
-    1. Check your amount
-    2. Add money
-    3. Withdraw money
-    =
-    ''')))
 
-    if function == 1:
-        Check_amount(amount)
-    elif function == 2:
-        add_money(amount)
-    elif function == 3:
-        withdraw(amount)
-    else: 
-        raise Exception("Please pick a valid number")
+    while True: 
+        try:
+            operation = int(input("\nDo you want to :\n1. Check your amount\n2. Add money\n3. Withdraw money \n4. Exit\n="))
 
-except Exception as ex:
-    print(f"Error: {ex}")
+            if operation == 1:
+                Check_amount()
+            elif operation == 2:
+                add_money()
+            elif operation == 3:
+                withdraw()
+            elif operation == 4:
+                break
+            else: 
+                raise Exception("Please pick a valid number")
 
-else : 
-    print("No Error")
+        except Exception as ex:
+            print(f"Error: {ex}")
 
-finally: 
-    print("Thank you for using our application")
+        else : 
+            print("Thank You")
+
+        finally: 
+            print("Thank you for using our application")
+
+if __name__ == "__main__" :
+    main()
